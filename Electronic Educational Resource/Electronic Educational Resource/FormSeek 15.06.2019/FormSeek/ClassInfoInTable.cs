b9445e10ea66bb17915a7ec029fed036fd4afd85
KovalenkoAlexandr <kovalencko96@gmail.com>
2020-11-22 16:30:15 +0200

    Create ClassInfoInTable.cs

---
+++ Electronic Educational Resource/Electronic Educational Resource/FormSeek 15.06.2019/FormSeek/ClassInfoInTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormSeek
+{
+	public class ClassInfoInTable
+	{
+		public int id_fio;
+		public string fio;
+		public int countSubject;
+		public int countMaterials;
+
+		public ClassInfoInTable() { id_fio = 0; fio = ""; countSubject = 0; countMaterials = 0; }
+	}
+}
