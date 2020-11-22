1651c4220e28dddaf6c73d597f54119cffd941fe
KovalenkoAlexandr <kovalencko96@gmail.com>
2020-11-22 16:07:11 +0200

    Create Discipline.cs

---
+++ Electronic Educational Resource/Electronic Educational Resource/Program/Discipline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithData
+{
+    internal class Discipline
+    {
+        private static string discipline_name;
+
+        public static string Discipline_name { get => discipline_name; set => discipline_name = value; }
+    }
+}
