884d9654f4e3656f99c5d76b8719956d15aa2be4
KovalenkoAlexandr <kovalencko96@gmail.com>
2020-11-22 16:30:16 +0200

    Create ClassCountElementsInTreeView.cs

---
+++ Electronic Educational Resource/Electronic Educational Resource/FormSeek 15.06.2019/FormSeek/ClassCountElementsInTreeView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormSeek
+{
+	public class ClassCountElementsInTreeView
+	{
+		public int countAllElements;
+		public int countSubject;
+		public int countName;
+		public int countTypeDocuments;
+		public int countDocuments;
+
+		public ClassCountElementsInTreeView()
+		{
+			countAllElements = 0;
+			countSubject = 0;
+			countName = 0;
+			countTypeDocuments = 0;
+			countDocuments = 0;
+		}
+	}
+}
