84464393c77f13af9b0b89d96e12e43b83412ac1
KovalenkoAlexandr <kovalencko96@gmail.com>
2020-11-22 15:55:35 +0200

    Create DBUtils.cs

---
+++ Electronic Educational Resource/Electronic Educational Resource/Shared/DBUtils.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Shared
+{
+    public class DBUtils
+    {
+            public static MySqlConnection GetDBConnection()
+            {
+                string connString = null;
+                connString = System.Configuration.ConfigurationManager.
+                         ConnectionStrings["ServerApplicationConnection"].ConnectionString;
+                MySqlConnection conn = new MySqlConnection(connString);
+                return conn;
+            }
+    }
+}
