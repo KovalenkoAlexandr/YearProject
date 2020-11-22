00dad843ee0d7680fe2da5550199ab64e6418ad8
KovalenkoAlexandr <kovalencko96@gmail.com>
2020-11-22 16:29:10 +0200

    Create InformationAboutProgram.cs

---
+++ Electronic Educational Resource/Electronic Educational Resource/FormSeek 15.06.2019/FormSeek/InformationAboutProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormSeek
+{
+	public partial class InformationAboutProgram : Form
+	{
+		public InformationAboutProgram()
+		{
+			InitializeComponent();
+		}
+
+		private void InformationAboutProgram_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void ToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+	}
+}
