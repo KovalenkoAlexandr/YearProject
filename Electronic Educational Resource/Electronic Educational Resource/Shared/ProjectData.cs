149da5065ac4974ee2478a2c5e3d435e5f74768b
KovalenkoAlexandr <kovalencko96@gmail.com>
2020-11-22 15:55:29 +0200

    Create ProjectData.cs

---
+++ Electronic Educational Resource/Electronic Educational Resource/Shared/ProjectData.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace Shared
+{
+    class ProjectData
+    {
+        internal static string GetProjectFolder()
+        {
+            //return AppDomain.CurrentDomain.BaseDirectory;//C:\Users\Алекс\Desktop\Електронний освітній ресурс\Диплом программа\bin\Debug
+            string directory = Directory.GetCurrentDirectory();//C:\Users\Алекс\Desktop\Електронний освітній ресурс\Диплом программа\bin\Debug
+            string parent = System.IO.Directory.GetParent(directory).FullName;
+            //return parent;
+            //MessageBox.Show(directory);
+            return directory;
+        }
+    }
+}
