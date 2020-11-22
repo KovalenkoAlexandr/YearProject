40ed45c80043fc1c9bca7e4bac2de53a35d557d2
KovalenkoAlexandr <kovalencko96@gmail.com>
2020-11-22 16:06:39 +0200

    Create InformationAboutDocumentAll.cs

---
+++ Electronic Educational Resource/Electronic Educational Resource/Program/InformationAboutDocumentAll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithData
+{
+	public class InformationAboutDocumentAll
+	{
+		public int id;
+
+		public string fio;
+		public string discipline;
+		public string type;
+		public string name;
+		public string expansion;
+		public string keyword;
+
+		public bool NewElement;
+
+		public InformationAboutDocumentAll() { }
+	}
+}
