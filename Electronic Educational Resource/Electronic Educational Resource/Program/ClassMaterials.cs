80b5456fe6136e3a886f87c754be0ec96fc95919
KovalenkoAlexandr <kovalencko96@gmail.com>
2020-11-22 16:08:17 +0200

    Create ClassMaterials.cs

---
+++ Electronic Educational Resource/Electronic Educational Resource/Program/ClassMaterials.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithData
+{
+	public class ClassMaterials
+	{
+		public string name;
+		public string discipline;
+		public string[] materials;
+		public int count;
+		public int countForTypeDoc;
+
+		public ClassMaterials()
+		{
+			name = "";
+			discipline = "";
+			materials = new string[20];
+			count = 0;
+			countForTypeDoc = 0;
+		}
+
+		public bool checkPrint(string tmp)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				if (materials[i] == tmp) return true;
+			}
+			return false;
+		}
+
+		public void printToMaterials(string tmp)
+		{
+			materials[count++] = tmp;
+		}
+	}
+}
