4c4ef91514bf2f9b38398a0d5ca0d21a597b0ff6
KovalenkoAlexandr <kovalencko96@gmail.com>
2020-11-22 16:06:37 +0200

    Create LanguageSets.cs

---
+++ Electronic Educational Resource/Electronic Educational Resource/Program/LanguageSets.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace WorkingWithData
+{
+   internal class LanguageSets
+    {
+        internal static void English()
+        {
+            InputLanguage.CurrentInputLanguage = 
+                InputLanguage.FromCulture(new System.Globalization.CultureInfo("en-US"));
+        }
+
+        internal static void Ukrainian()
+        {
+            InputLanguage.CurrentInputLanguage =
+                InputLanguage.FromCulture(new System.Globalization.CultureInfo("uk-UA"));
+        }
+    }
+}
