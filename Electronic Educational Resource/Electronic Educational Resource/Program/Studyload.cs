2562c0d67e7a739c3d0ed96fce3e8163916ff437
KovalenkoAlexandr <kovalencko96@gmail.com>
2020-11-22 15:57:17 +0200

    Create Studyload.cs

---
+++ Electronic Educational Resource/Electronic Educational Resource/Program/Studyload.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace WorkingWithData
+{
+    internal class Studyload
+    {
+        private static int studyloadid;
+        private static string olduser;
+        private static string newuser;
+        private static List<string> discipline = new List<string>();
+        private static List<string> teacher = new List<string>();
+
+        internal static string Olduser { get => olduser; set => olduser = value; }
+        internal static string Newuser { get => newuser; set => newuser = value; }
+        internal static int Studyloadid { get => studyloadid; set => studyloadid = value; }
+        internal static List<string> Discipline { get => discipline; set => discipline = value; }
+        internal static List<string> Teacher { get => teacher; set => teacher = value; }
+    }
+}
